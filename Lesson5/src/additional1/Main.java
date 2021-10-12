e9f5abe5aa91d4d2d6d9c1444c0d292d99367897
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-12 20:56:06 +0300

    add additional1

--- Lesson5/src/additional1/Main.java
+++ Lesson5/src/additional1/Main.java
@@ -1,4 +1,23 @@
 package additional1;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> teachers = new ArrayList<>();
+        teachers.add("Генадий Свердлов");
+        teachers.add("Татьяна Баринова");
+        teachers.add("Наталья Шпадарева");
+        teachers.add("Best Teacher");
+        teachers.add("Bad Teacher");
+
+        System.out.println("Индекс лучшего учителя");
+        System.out.println(teachers.indexOf("Best Teacher"));
+        System.out.println("Индекс худшего учителя");
+        System.out.println(teachers.indexOf("Bad Teacher"));
+        System.out.println(teachers.toString());
+
+    }
+
+
 }
