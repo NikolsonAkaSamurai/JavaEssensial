11faf66c588e010499833c75407e3cd0363aa55d
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-14 20:51:20 +0300

    laboratorka2

---
+++ OOP_6/src/com/laboratorka2/Main.java
@@ -0,0 +1,10 @@
+package com.laboratorka2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("john", "354546");
+        user.createQuery();
+
+    }
+}
---
+++ OOP_6/src/com/laboratorka2/User.java
@@ -0,0 +1,29 @@
+package com.laboratorka2;
+
+import java.util.Queue;
+
+public class User {
+    private String login;
+    private String password;
+
+    User(){}
+
+    User(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    public void createQuery(){
+        class Query{
+            public void printToLog(){
+                System.out.println("пользователь "+login+" пароль "+password);
+            }
+        }
+        Query query = new Query();
+        query.printToLog();
+    }
+
+
+
+
+}
