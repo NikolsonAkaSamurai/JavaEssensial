398b42c9d05bfc9091c92442ccdc6863a0b1e07d
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-23 18:30:48 +0300

    final rework task 2

--- OOP7/src/com/company/task2/Main.java
+++ OOP7/src/com/company/task2/Main.java
@@ -2,7 +2,16 @@ package com.company.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Animals animal = Animals.BIRD;
-        System.out.println(animal.toString());
+        Animals[] animal = new Animals[3];
+        
+        animal[0] = Animals.DOG;
+        animal[1] = Animals.CAT;
+        animal[2] = Animals.BIRD;
+
+        for (Animals temp: animal) {
+            System.out.println(temp.toString());
+        }
+
+
     }
 }
