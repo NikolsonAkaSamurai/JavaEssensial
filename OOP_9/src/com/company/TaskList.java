a34fac627e743868f4d2f22a45e23a6596486b8d
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-25 04:03:27 +0300

    OOP_9/add taskList

--- OOP_9/src/com/company/TaskList.java
+++ OOP_9/src/com/company/TaskList.java
@@ -3,6 +3,35 @@ package com.company;
 public class TaskList {
 
     /*
+    Задание 1
+    В любой из профильных книг (Хорстман, Эккель) найти соответствующие темы и закрепить материал.
+    Использование YouTube, Quizful приветствуется.
+
+    Задание 2
+    Создать классы:
+    1) Основной класс Device (manufacturer(String), price(float), serialNumber(String));
+    2) Сабкалсс Monitor (resolutionX(int), resolutionY(int)) and EthernetAdapter (speed (int), mac (String));
+    Добавить методы доступа. Конструктор.
+
+    Задание 3
+    Смотреть задание 2.
+    В обоих классах переопределить метод toString, что бы вывод был следующим:
+    Device: manufacturer =Samsung, price=120, serialNumber=AB1234567CD
+    Monitor: manufacturer =Samsung, price=120, serialNumber=AB1234567CD, X=1280,Y=1024
+
+    Задание 4
+    Смотреть задание 2.
+    Переопределить методы equals & hashCode в каждом классе.
+    Создать класс Main, в котором создать объекты классов и продемонстрировать переопределенные
+    методы.
+    
+    Дополнительное
+    Используя Intelij IDEA создать проект, пакет.
+    Создать класс Animal с именем String, возрастом int, хвостом Boolean. В классе переопределить метод
+    toString, что бы вывод был сдедующим
+    «Имя: Васька, возраст: 45, хвост: нет».
+    В классе Animal переопределить методы equals & hashCode.
+
     Лабораторка:
     Создать класс Car со следующими полями:
     • id,
