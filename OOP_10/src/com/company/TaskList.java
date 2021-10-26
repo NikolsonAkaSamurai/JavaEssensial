7c0a79599846d72fc917cc5d6caba652c5b18592
Nikolai Syuris <nik.syu34@gmail.com>
2021-10-26 20:05:45 +0300

    add folder OOP_10

---
+++ OOP_10/src/com/company/TaskList.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TaskList {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ OOP_10/src/com/company/laboratorka/Main.java
@@ -0,0 +1,4 @@
+package com.company.laboratorka;
+
+public class Main {
+}
